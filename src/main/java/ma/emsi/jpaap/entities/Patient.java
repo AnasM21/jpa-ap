5c4848ad68678ea0651496382e7a76073c841139
BENERRAZAM_MOHAMMADI <mohammadi_anas@emsi-edu.ma>
2022-03-24 20:14:47 +0100

    24/03/22

--- src/main/java/ma/emsi/jpaap/entities/Patient.java
+++ src/main/java/ma/emsi/jpaap/entities/Patient.java
@@ -6,10 +6,7 @@ import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
@@ -17,7 +14,9 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 50)
     private String nom;
+    @Temporal(TemporalType.DATE)
     private Date dateNaissance;
     private boolean malade;
     private int score;
